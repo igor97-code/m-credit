a73af3f88a2487ee081eea1554821a6a6cad320a
uuusuuuqa <ronai.lemon@gmail.com>
2019-12-06 13:05:47 +0400

    push

--- app/js/main.js
+++ app/js/main.js
@@ -620,3 +620,35 @@ function initSeoMacbookSlider() {
         prevSlideMessage: '',
     });
 }
+$(function () {
+    if ($('[data-tabs-block]').length) {
+        $('[data-tabs-block]').each(function() {
+            initTabs($(this));
+        })
+    }
+});
+
+
+function initTabs($tabs) {
+    if (!$tabs) return;
+
+    var defaultTab = $tabs.attr('data-default-tab');
+    if (!defaultTab) {
+        defaultTab = $tabs.find('[data-tab]').first().attr('data-tab');
+    }
+    
+    changeTab(defaultTab);
+
+    $tabs.on('click', '.switch', function() {
+        if ($(this).hasClass('active')) return false;
+
+        changeTab($(this).attr('data-tab'));
+        
+        return false;
+    });
+
+    function changeTab(tab) {
+        $tabs.find('[data-tab]').removeClass('active');
+        $tabs.find('[data-tab="' + tab + '"]').addClass('active');
+    }
+}
